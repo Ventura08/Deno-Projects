2f601df8dcde231f2abf3b0caaa5314baa91c8b4
Estevao Boaventura <estevao.boaventura@educat.com.br>
2023-03-30 15:57:56 -0300

    cpf maker

--- src/cpfMaker/cpfMaker.ts
+++ src/cpfMaker/cpfMaker.ts
@@ -1,12 +1,15 @@
-const generateRandomNumbers =  (range: number, quantity: number, index: number, numbers:string): string => {
-    if(index == quantity - 1){
-        return numbers
-    }
-    numbers += Math.floor((Math.random() * range))
-    index++
-    return generateRandomNumbers(range, quantity, index, numbers)
-}
-const teste = generateRandomNumbers(10, 10, 0, '') satisfies string
-console.log(teste)
+// const generateRandomNumbers =  (range: number, quantity: number, index: number, numbers:string): string => {
+//     if(index == quantity - 1){
+//         return numbers
+//     }
+//     numbers += Math.floor((Math.random() * range))
+//     index++
+//     return generateRandomNumbers(range, quantity, index, numbers)
+// }
+// generateRandomNumbers(10, 10, 0, '') satisfies string
+const numbers = '123456789' satisfies string
+Number(numbers.charAt(2)) * (Number(numbers.indexOf('3')) + 1)
+// const createFirstCheckNumber = (numbers: string) => {
+//     // PRECISO PEGAR CADA UM DOS DIGITOS E MULTIPLICAR PELA POSICAO DELE
+// }
 
-// Math.floor((Math.random() * range['max']))
