c70cd9f008764eddb01e9e829e82029cb0f297c5
Ventura08 <estevaotevu08@gmail.com>
2023-03-31 13:17:02 -0300

    Custom message

--- src/cpfMaker/cpfMaker.ts
+++ src/cpfMaker/cpfMaker.ts
@@ -1,15 +1,42 @@
-// const generateRandomNumbers =  (range: number, quantity: number, index: number, numbers:string): string => {
-//     if(index == quantity - 1){
-//         return numbers
-//     }
-//     numbers += Math.floor((Math.random() * range))
-//     index++
-//     return generateRandomNumbers(range, quantity, index, numbers)
-// }
-// generateRandomNumbers(10, 10, 0, '') satisfies string
-const numbers = '123456789' satisfies string
-Number(numbers.charAt(2)) * (Number(numbers.indexOf('3')) + 1)
-// const createFirstCheckNumber = (numbers: string) => {
-//     // PRECISO PEGAR CADA UM DOS DIGITOS E MULTIPLICAR PELA POSICAO DELE
-// }
+const createFirstCheckNumber = (cpfNumbers: string, sum: number, weight: number): number => {
+    if(!(cpfNumbers)){
+        return (sum % 11 == 10) ? 0 : (sum % 11)
+    }
+    return createFirstCheckNumber(
+        (cpfNumbers.substring(1)), 
+        (sum += Number(cpfNumbers.charAt(0)) * weight), 
+        (weight + 1)
+    )
+}
+
+const createSecondCheckNumber = (cpfNumbers: string, sum: number, weight: number): number => {
+    if(!(cpfNumbers)){
+        return (sum % 11 == 10) ? 0 : (sum % 11)
+    }
+    return createFirstCheckNumber(
+        (cpfNumbers.substring(1)), 
+        (sum += Number(cpfNumbers.charAt(0)) * weight), 
+        (weight + 1)
+    )
+}
+
+
+const generateRandomNumbers =  (index: number, numbers:string): string => {
+    if(index == 10 - 1){
+        return numbers
+    }
+    numbers += Math.floor((Math.random() * 10))
+    index++
+    return generateRandomNumbers(index, numbers)
+}
+
+const generateCpf = (formated: boolean) => {
+    const cpfNumbers = generateRandomNumbers(0, '') satisfies string
+    const firstCheckNumber = createFirstCheckNumber(cpfNumbers, 0, 1) satisfies number
+    const secondCheckNumber = createSecondCheckNumber(cpfNumbers + firstCheckNumber, 0, 0) satisfies number
+    const finalValue = cpfNumbers + firstCheckNumber + secondCheckNumber;
+    return (formated) ? finalValue.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, "$1.$2.$3-$4") : finalValue
+}
+
+generateCpf(true)
 
